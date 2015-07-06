c134b9d23357c57ab49dc79c2a7680f925746ed5
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-06 14:19:40 -0700

    Initial setup of index.html and heap.js files.

---
+++ heap.js
@@ -0,0 +1,14 @@
+// Heap visualization
+// Authors: Katrina Uychaco && Kalev Roomann-Kurrik
+// Last Modified: Monday July 6, 2015
+
+// TODO: array to represent input data
+
+// TODO: create Heap constructor
+
+// TODO: insertion method on prototype
+
+// TODO: function to transform heap array into a nested d3 data structure
+
+// TODO: write d3 code for tree visualization
+
